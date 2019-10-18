79a8ed28f1e0fcb0fa8e09d5c30213d9c59c583d
Martin Farkas <martin.farkas@plus4u.net>
2019-10-18 06:43:42 +0200

    Create exercise 3

--- hooked-on-hooks/src/bricks/Lights.js
+++ hooked-on-hooks/src/bricks/Lights.js
@@ -1,79 +1,12 @@
-import React, { useRef, useReducer, useEffect, useContext } from "react";
-import HueContext from "../contexts/HueContext";
+import React, { useState } from "react";
 import LightList from "./LightList";
-import LightReducer from "../reducers/LightReducer";
 
 function Lights() {
-  const hueContext = useContext(HueContext);
-  const [lights, dispatch] = useReducer(LightReducer, []);
-  const timerId = useRef(null);
+  const [lights] = useState([]);
 
-  useEffect(() => {
-    async function loadLights() {
-      if (hueContext.status !== "ready") {
-        return;
-      }
+  // TODO Load and convert lights from Hue bridge via HueContext.user.getLights
 
-      const response = await hueContext.user.getLights();
-
-      const newLights = Object.keys(response).map(key => {
-        let light = response[key];
-        light.id = key;
-        return light;
-      });
-
-      dispatch({ type: "reset", payload: newLights });
-    }
-
-    timerId.current = setInterval(loadLights, 1000);
-
-    loadLights();
-
-    return () => clearInterval(timerId.current);
-  }, [hueContext]);
-
-  function dispatchWrapper(dispatch) {
-    return action => {
-      const { payload } = action;
-
-      switch (action.type) {
-        case "toggleOn":
-          hueContext.user.setLightState(payload.light.id, {
-            on: payload.on
-          });
-          break;
-        case "setBrightness":
-          hueContext.user.setLightState(payload.light.id, {
-            bri: payload.bri
-          });
-          break;
-        case "setHue":
-          hueContext.user.setLightState(payload.light.id, { hue: payload.hue });
-          break;
-        case "setSaturation":
-          hueContext.user.setLightState(payload.light.id, { sat: payload.sat });
-          break;
-        case "setColor":
-          hueContext.user.setLightState(payload.light.id, payload.color);
-          break;
-        case "setEffect":
-          hueContext.user.setLightState(payload.light.id, {
-            effect: payload.effect
-          });
-          break;
-        default:
-          break;
-      }
-
-      dispatch(action);
-    };
-  }
-
-  if (hueContext.status === "connecting") {
-    return "Connecting to bridge...";
-  }
-
-  return <LightList lights={lights} onDispatch={dispatchWrapper(dispatch)} />;
+  return <LightList lights={lights} />;
 }
 
 export default Lights;
