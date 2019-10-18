7bc2864bad3b268f157559974b55cd7e8015f2a4
Martin Farkas <martin.farkas@plus4u.net>
2019-10-18 06:34:38 +0200

    Create exercise 2

--- hooked-on-hooks/src/bricks/BridgeForm.js
+++ hooked-on-hooks/src/bricks/BridgeForm.js
@@ -1,24 +1,17 @@
-/** @jsx jsx */
-import { jsx, css } from "@emotion/core";
-import { useContext, useState } from "react";
-import HueContext from "../contexts/HueContext";
+import React, { useState } from "react";
 
 export default function BridgeForm() {
   const [bridgeIp, setBridgeIp] = useState("127.0.0.1");
-  const { addBridge } = useContext(HueContext);
 
   function _handleChange(e) {
     setBridgeIp(e.target.value);
   }
 
-  function _handleClick() {
-    addBridge({ ip: bridgeIp });
-  }
+  // TODO Add button and  on click call HueContext.addBridge
 
   return (
     <div>
       <input type="text" value={bridgeIp} onChange={_handleChange} />
-      <button onClick={_handleClick}>Connect</button>
     </div>
   );
 }
